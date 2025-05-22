79b5deb22bcb509ab85448fd3c0e741f97909f22
R3vo <robertseibel@outlook.de>
2025-05-22 20:39:48 +0200

    several improvements to config.toml parsing

--- sortPDFByPageSize.py
+++ sortPDFByPageSize.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 from pypdf import PdfReader
 from tkinter import filedialog
@@ -50,60 +49,88 @@ def iterateFiles(strRoot):
     for oFile in list(Path(strRoot).glob('*.pdf')):
         strFileName = oFile.name
 
-    # try:
-        if (bHandleBOMs):
-            if ('.bom.' in strFileName or '.bomall.' in strFileName or '.bomstrc.' in strFileName):
-                moveOrCopyFile(oFile.__str__(), bMoveFiles, strFolderNameBOM, strRoot)
-                continue
-
-        aSize = [-1, -1]
-        bDifferentSizes = False
-
-        for page in PdfReader(oFile).pages:
-            boundingBox = page.trimbox
-            iWidth = round(boundingBox.width)
-            iHeight = round(boundingBox.height)
-
-            # Set aSize initially
-            if (aSize[0] == -1 or aSize[1] == -1):
-                aSize[0] = iWidth
-                aSize[1] = iHeight
-            else:
-                # If new iWidth and iHeight are different
-                # from previous iWidth and iHeight
-                # then pages don't have the same aSize
-                if (aSize[0] != iWidth or aSize[1] != iHeight):
-                    bDifferentSizes = True
+        try:
+            if (bHandleBOMs):
+                if ('.bom.' in strFileName or '.bomall.' in strFileName or '.bomstrc.' in strFileName):
+                    moveOrCopyFile(oFile.__str__(), bMoveFiles, strFolderNameBOM, strRoot)
                     continue
 
-        bFileHandled = False
+            aSize = [-1, -1]
+            bDifferentSizes = False
 
-        # Move files to strFolderNameUnknown folder if
-        # page sizes are different
-        if (not bDifferentSizes):
-            for aSizePreset in dictSizes.values():
-                if (areListsEqualWithError(aSize, [aSizePreset[0], aSizePreset[1]], iSizeTolerance)):
-                    moveOrCopyFile(oFile.__str__(), bMoveFiles, aSizePreset[2], strRoot)
-                    bFileHandled = True
-                    break
+            for page in PdfReader(oFile).pages:
+                boundingBox = page.trimbox
+                iWidth = round(boundingBox.width)
+                iHeight = round(boundingBox.height)
 
-        if (bDifferentSizes or not bFileHandled):
-            moveOrCopyFile(oFile.__str__(), bMoveFiles, strFolderNameUnknown, strRoot)
-            bFileHandled = True
-            continue
-    # except:
-        oLog.error(f'{strFileName} could not be opened!'),
+                # Set aSize initially
+                if (aSize[0] == -1 or aSize[1] == -1):
+                    aSize[0] = iWidth
+                    aSize[1] = iHeight
+                else:
+                    # If new iWidth and iHeight are different
+                    # from previous iWidth and iHeight
+                    # then pages don't have the same aSize
+                    if (aSize[0] != iWidth or aSize[1] != iHeight):
+                        bDifferentSizes = True
+                        continue
+
+            bFileHandled = False
+
+            # Move files to strFolderNameUnknown folder if
+            # page sizes are different
+            if (not bDifferentSizes):
+                for aSizePreset in dictSizes.values():
+                    if (areListsEqualWithError(aSize, [aSizePreset[0], aSizePreset[1]], iSizeTolerance)):
+                        moveOrCopyFile(oFile.__str__(), bMoveFiles, aSizePreset[2], strRoot)
+                        bFileHandled = True
+                        break
+
+            if (bDifferentSizes or not bFileHandled):
+                moveOrCopyFile(oFile.__str__(), bMoveFiles, strFolderNameUnknown, strRoot)
+                bFileHandled = True
+                continue
+        except:
+            oLog.error(f'{strFileName} could not be opened!'),
 
 if (__name__ == '__main__'):
 
     oLog = logging.getLogger(__name__)
     logging.basicConfig(filename='logfile.log', level=logging.INFO)
 
+    # Check if config.toml exists
     if (not Path('config.toml').exists()):
         messagebox.showerror('config.toml not found',
                              'The configuration oFile config.toml is missing! Program aborted!')
         oLog.error('The configuration oFile config.toml is missing! Program aborted!')
         exit()
+
+    # Load config.toml
+    with open("config.toml", "rb") as f:
+        data = tomllib.load(f)
+
+    # Check for valid config.toml
+    if (data.get('SIZES') == None):
+        messagebox.showerror('SIZES not found',
+                        'config.toml does not contain sizes. Program aborted!')
+        oLog.error('config.toml does not contain sizes. Program aborted!')
+        exit()
+
+    if (data.get('OPTIONS') == None):
+        messagebox.showerror('OPTIONS not found',
+                        'config.toml does not contain options. Program aborted!')
+        oLog.error('config.toml does not contain options. Program aborted!')
+        exit()
+
+    dictSizes = data.get('SIZES')
+    dictOptions = data.get('OPTIONS')
+
+    # Use default values if not defined in config.toml
+    bHandleBOMs = dictOptions.get('handleBomFiles', True)
+    bMoveFiles = dictOptions.get('moveFiles', False)
+    iSizeTolerance = dictOptions.get('sizeTolerance', 10)
+    strFolderNameBOM = dictOptions.get('bom', "@BOMs")
+    strFolderNameUnknown = dictOptions.get('unknownSize', "@manuelSortingRequired")
 
     strRoot = pickFolder()
 
@@ -112,23 +139,8 @@ if (__name__ == '__main__'):
                                         f'Are you sure you want to use this path? {strRoot}')
 
         if (strResult == 'yes'):
-            # Store settings globally
-            with open("config.toml", "rb") as f:
-                data = tomllib.load(f)
-
-            dictSizes = dict()
-
-            for aSize in data.get('SIZES'):
-                dictSizes[aSize] = data['SIZES'][aSize]
-
-            strFolderNameUnknown = data['OPTIONS']['unknownSize']
-            strFolderNameBOM = data['OPTIONS']['bom']
-            bHandleBOMs = data['OPTIONS']['handleBomFiles']
-            iSizeTolerance = data['OPTIONS']['sizeTolerance']
-            bMoveFiles = data['OPTIONS']['move']
-
             iterateFiles(strRoot)
         else:
-            oLog.info('Copy operation aborted!')
+            oLog.info('Copy operation aborted by user!')
     else:
         oLog.info('Copy operation canceled. Not valid folder was selected!')
